aa75f949c5ddbcef763ca7bd1243f3696cd2440a
Viktor Marchuk <vm_h@mail.ru>
2023-07-04 15:54:46 +0300

    added task 52

--- Program.cs
+++ Program.cs
@@ -119,7 +119,7 @@ FindElement(myArray, elementArray);
 */
 
 //variant 2 - порядковый номер
-
+/*
 int InputNum(string message)
 {
     Console.Write(message);
@@ -171,7 +171,7 @@ int[,] myArray = Create2DArray(rows, columns);
 Fill2DArray(myArray, minValue, maxValue);
 Print2DArray(myArray);
 FindElement(myArray, elementArray);
-
+*/
 
 
 /*
@@ -184,6 +184,53 @@ FindElement(myArray, elementArray);
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 */
 
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
 
+int[,] Create2DArray(int rows, int cols)
+{
+    return new int[rows, cols];
+}
 
+void Fill2DArray(int[,] array, int min, int max)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = rnd.Next(min, max + 1);
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]}\t");
+        Console.WriteLine();
+    }
+}
+
+void ArithmeticMean(int[,] array)
+{
+    Console.Write($"Среднее арифметическое каждого столбца: ");
+    double average = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++) average += array[i, j];
+        Console.Write($"{Math.Round(average / (array.GetLength(0)), 2)}; ");
+        average = 0;
+    }
+}
+
+int rows = InputNum("Введите количество строк: ");
+int columns = InputNum("Введите количество столбцов: ");
+int minValue = InputNum("Введите минимальное значение диапазона: ");
+int maxValue = InputNum("Введите максимальное значение диапазона: ");
+int[,] myArray = Create2DArray(rows, columns);
+Fill2DArray(myArray, minValue, maxValue);
+Print2DArray(myArray);
+ArithmeticMean(myArray);
 
